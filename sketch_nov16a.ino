2844b5fbbee897e463b1e07bc7cbe6d4a7b2c4a5
Akhil <skippynose2@users.noreply.github.com>
2017-12-05 07:32:48 -0500

    Add files via upload

---
+++ sketch_nov16a.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <Servo.h>
+
+LiquidCrystal_I2C lcd(0X27,16,2);
+int sensor = A0;
+Servo myServo;
+int servoPin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin();
+  Serial.begin(9600);
+  myServo.attach(servoPin);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  int readVal = analogRead(sensor);
+  Serial.println(readVal);
+  lcd.clear();
+  lcd.print(readVal);
+  if (readVal >= 600)
+  {
+    myServo.write(180);
+  }else if (readVal <= 500) 
+  {
+    myServo.write(0);  
+  }
+
+}
